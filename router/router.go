aa024241a0fcf3bd6ee030a5f4d5f20371375b82
gainong <gainong@micous.com>
2025-11-03 11:50:37 +0800

    新增数学裁剪接口，支持代理请求处理

--- router/router.go
+++ router/router.go
@@ -49,6 +49,7 @@ func SetupRouter(clientRepo repository.ClientRepository, callLogRepo repository.
 		api.POST("/essay/evaluate/english", proxyHandler.ProxyRequest)
 		api.POST("/math/quesiton_similar_recommend", proxyHandler.ProxyRequest)
 		api.POST("/math/process", proxyHandler.ProxyRequest)
+		api.POST("/math/cropping", proxyHandler.ProxyRequest)
 	}
 
 	admin := r.Group("/admin")
